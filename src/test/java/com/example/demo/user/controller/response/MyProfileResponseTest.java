3a8d221447d2ab2956c44d73d1a0a2707c97eb4d
joona95 <joona95@naver.com>
2023-07-24 20:51:15 +0900

    test: 도메인과 영속성 객체 구분에 따른 테스트코드 추가 및 Clock, Uuid 처리

--- src/test/java/com/example/demo/user/controller/response/MyProfileResponseTest.java
+++ src/test/java/com/example/demo/user/controller/response/MyProfileResponseTest.java
@@ -1,13 +1,32 @@
 package com.example.demo.user.controller.response;
 
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MyProfileResponseTest {
 
     @Test
     public void User으로_응답을_생성할_수_있다() {
         //given
+        User user = User.builder()
+                .id(1L)
+                .email("kok202@naver.com")
+                .nickname("kok202")
+                .address("Seoul")
+                .status(UserStatus.ACTIVE)
+                .lastLoginAt(100L)
+                .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa")
+                .build();
         //when
+        MyProfileResponse myProfileResponse = MyProfileResponse.from(user);
         //then
+        assertThat(myProfileResponse.getId()).isEqualTo(1L);
+        assertThat(myProfileResponse.getEmail()).isEqualTo("kok202@naver.com");
+        assertThat(myProfileResponse.getAddress()).isEqualTo("Seoul");
+        assertThat(myProfileResponse.getStatus()).isEqualTo(UserStatus.ACTIVE);
+        assertThat(myProfileResponse.getLastLoginAt()).isEqualTo(100L);
     }
 }
