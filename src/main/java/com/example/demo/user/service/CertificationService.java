f016ea42362311addb1e13cdfd3d46ad48ba3bbe
joona95 <joona95@naver.com>
2023-07-24 14:23:06 +0900

    refactor: MailSender에 의존성 역전 적용

--- src/main/java/com/example/demo/user/service/CertificationService.java
+++ src/main/java/com/example/demo/user/service/CertificationService.java
@@ -1,5 +1,6 @@
 package com.example.demo.user.service;
 
+import com.example.demo.user.service.port.MailSender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,17 +10,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CertificationService {
 
-    private final JavaMailSender mailSender;
+    private final MailSender mailSender;
 
     public void send(String email, long userId, String certificationCode) {
         String certificationUrl = generateCertificationUrl(userId, certificationCode);
         String title = "Please certify your email address";
         String content = "Please click the following link to certify your email address: " + certificationUrl;
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject(title);
-        message.setText(content);
-        mailSender.send(message);
+        mailSender.send(email, title, content);
     }
 
     private String generateCertificationUrl(long userId, String certificationCode) {
