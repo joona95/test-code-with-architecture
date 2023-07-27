2c44deb09ffd19feb6a63ee694811c040b818f03
joona95 <joona95@naver.com>
2023-07-27 21:44:52 +0900

    test: Fake를 이용한 PostService 테스트 작성

--- src/test/java/com/example/demo/post/domain/PostTest.java
+++ src/test/java/com/example/demo/post/domain/PostTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.post.domain;
 
+import com.example.demo.mock.TestClockHolder;
 import com.example.demo.user.domain.User;
 import com.example.demo.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
@@ -25,13 +26,43 @@ public class PostTest {
                 .build();
 
         //when
-        Post post = Post.from(writer, postCreate);
+        Post post = Post.from(writer, postCreate, new TestClockHolder(1678530673958L));
         //then
         assertThat(post.getContent()).isEqualTo("helloworld");
+        assertThat(post.getCreatedAt()).isEqualTo(1678530673958L);
         assertThat(post.getWriter().getEmail()).isEqualTo("kok202@naver.com");
         assertThat(post.getWriter().getNickname()).isEqualTo("kok202");
         assertThat(post.getWriter().getAddress()).isEqualTo("Seoul");
         assertThat(post.getWriter().getStatus()).isEqualTo(UserStatus.ACTIVE);
         assertThat(post.getWriter().getCertificationCode()).isEqualTo("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+    }
+
+    @Test
+    public void PostUpdate로_게시물을_수정할_수_있다() {
+        //given
+        PostUpdate postUpdate = PostUpdate.builder()
+                .content("foobar")
+                .build();
+        User writer = User.builder()
+                .id(1L)
+                .email("kok202@naver.com")
+                .nickname("kok202")
+                .address("Seoul")
+                .status(UserStatus.ACTIVE)
+                .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa")
+                .build();
+        Post post = Post.builder()
+                .id(1L)
+                .content("helloworld")
+                .createdAt(1678530673958L)
+                .modifiedAt(0L)
+                .writer(writer)
+                .build();
+
+        //when
+        post = post.update(postUpdate, new TestClockHolder(1678530673958L));
+        //then
+        assertThat(post.getContent()).isEqualTo("foobar");
+        assertThat(post.getCreatedAt()).isEqualTo(1678530673958L);
     }
 }
