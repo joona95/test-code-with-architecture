8c08c94f545fd9ed8693005893f04d1a4e2ac316
joona95 <joona95@naver.com>
2023-07-27 20:59:51 +0900

    rename: 중형 테스트 패키지 이동

--- src/test/java/com/example/demo/medium/UserJpaRepositoryTest.java
+++ src/test/java/com/example/demo/medium/UserJpaRepositoryTest.java
@@ -1,10 +1,12 @@
-package com.example.demo.user.infrastructure;
+package com.example.demo.medium;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.example.demo.user.domain.UserStatus;
 import java.util.Optional;
 
+import com.example.demo.user.infrastructure.UserEntity;
+import com.example.demo.user.infrastructure.UserJpaRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
