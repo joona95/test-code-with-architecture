0d1ce5f3ab635377c04e0ac55889481f65e4e707
joona95 <joona95@naver.com>
2023-07-27 21:58:49 +0900

    refactor: Service를 추상화하여 Controller에서 참조하도록 의존성 역전 적용

--- src/test/java/com/example/demo/user/service/UserServiceTest.java
+++ src/test/java/com/example/demo/user/service/UserServiceTest.java
@@ -18,16 +18,16 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class UserServiceTest {
 
-    private UserService userService;
+    private UserServiceImpl userService;
 
     @BeforeEach
     void init() {
         FakeMailSender fakeMailSender = new FakeMailSender();
         FakeUserRepository fakeUserRepository = new FakeUserRepository();
-        this.userService = UserService.builder()
+        this.userService = UserServiceImpl.builder()
                 .uuidHolder(new TestUuidHolder("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab"))
                 .clockHolder(new TestClockHolder(1678530673958L))
-                .certificationService(new CertificationService(fakeMailSender))
+                .certificationService(new CertificationServiceImpl(fakeMailSender))
                 .userRepository(fakeUserRepository)
                 .build();
 
